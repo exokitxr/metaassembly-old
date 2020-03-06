e750712c34f90b83ec8f26f7d6ffaece3c451467
Avaer Kazmer <a@modules.io>
2020-03-06 18:24:55 -0500

    Hook in stdio flow

--- src/avrenderer/avrenderer_main.cpp
+++ src/avrenderer/avrenderer_main.cpp
@@ -45,70 +45,15 @@ using json = nlohmann::json;
 using Base64 = macaron::Base64;
 
 std::string logSuffix = "_native";
-HWND g_hWnd = NULL;
+// HWND g_hWnd = NULL;
 // CHAR s_szDllPath[MAX_PATH] = "vrclient_x64.dll";
-extern std::string dllDir;
+std::string dllDir;
 
-DWORD chromePid = 0;
-HWND chromeHwnd = NULL;
-
-char kProcess_SetIsVr[] = "IVRCompositor::kIVRCompositor_SetIsVr";
-char kProcess_SetTransform[] = "IVRCompositor::kIVRCompositor_SetTransform";
-char kProcess_PrepareBindSurface[] = "IVRCompositor::kIVRCompositor_PrepareBindSurface";
-char kProcess_SetDepthRenderEnabled[] = "IVRCompositor::kIVRCompositor_SetDepthRenderEnabled";
-char kProcess_SetQrEngineEnabled[] = "IVRCompositor::kIVRCompositor_SetQrEngineEnabled";
-char kProcess_GetQrCodes[] = "IVRCompositor::GetQrCodes";
-char kProcess_SetCvEngineEnabled[] = "IVRCompositor::kIVRCompositor_SetCvEngineEnabled";
-char kProcess_GetCvFeature[] = "IVRCompositor::GetCvFeature";
-char kProcess_AddCvFeature[] = "IVRCompositor::AddCvFeature";
-char kProcess_Terminate[] = "Process::Terminate";
-
-class HwndSearchStruct {
-public:
-  const std::string &s;
-  HWND hwnd;
-};
-BOOL CALLBACK enumWindowsProc(
-  __in HWND hwnd,
-  __in LPARAM lParam
-) {
-  HwndSearchStruct &o = *((HwndSearchStruct *)lParam);
-
-  CHAR windowTitle[1024];
-  GetWindowTextA(hwnd, windowTitle, sizeof(windowTitle));
-  
-  std::cout << "get window title: " << windowTitle << std::endl;
-
-  if (o.s == windowTitle) {
-    o.hwnd = hwnd;
-    return false;
-  } else {
-    return true;
-  }
-}
-/* HWND getHwndFromPid(DWORD pid) {
-  HwndSearchStruct o{
-    pid,
-    (HWND)NULL
-  };
-  EnumWindows(enumWindowsProc, (LPARAM)&o);
-  return o.hwnd;
-} */
-HWND getHwndFromTitle(const std::string &s) {
-  HwndSearchStruct o{
-    s,
-    (HWND)NULL
-  };
-  EnumWindows(enumWindowsProc, (LPARAM)&o);
-  return o.hwnd;
-}
+size_t ids = 0;
 
 inline uint32_t divCeil(uint32_t x, uint32_t y) {
   return (x + y - 1) / y;
 }
-
-size_t ids = 0;
-
 constexpr uint32_t chunkSize = 1000*1000;
 void respond(const json &j) {
   std::string outString = j.dump();
@@ -140,85 +85,138 @@ void respond(const json &j) {
 int main(int argc, char **argv) {
   tools::initLogs();
   
-  std::cout << "Start" << std::endl;
+  getOut() << "Start" << std::endl;
   
   std::unique_ptr<CAardvarkCefApp> app(new CAardvarkCefApp());
-
   std::thread renderThread([&]() -> void {
     while (!app->wantsToQuit()) {
       app->runFrame();
       std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
   });
-  
-  setmode(fileno(stdout), O_BINARY);
-  setmode(fileno(stdin), O_BINARY);
 
-  freopen(NULL, "rb", stdin);
-  freopen(NULL, "wb", stdout);
+  {  
+    char cwdBuf[MAX_PATH];
+    if (!GetCurrentDirectory(sizeof(cwdBuf), cwdBuf)) {
+      getOut() << "failed to get current directory" << std::endl;
+      abort();
+    }
+    dllDir = cwdBuf;
+    dllDir += "\\";
+    {
+      std::string manifestTemplateFilePath = std::filesystem::weakly_canonical(std::filesystem::path(dllDir + std::string(R"EOF(..\..\..\avrenderer\native-manifest-template.json)EOF"))).string();
+      std::string manifestFilePath = std::filesystem::weakly_canonical(std::filesystem::path(dllDir + std::string(R"EOF(\native-manifest.json)EOF"))).string();
 
-  char cwdBuf[MAX_PATH];
-  if (!GetCurrentDirectory(
-    sizeof(cwdBuf),
-    cwdBuf
-  )) {
-    std::cout << "failed to get current directory" << std::endl;
-    abort();
+      std::string s;
+      {
+        std::ifstream inFile(manifestTemplateFilePath);
+        s = std::string((std::istreambuf_iterator<char>(inFile)), std::istreambuf_iterator<char>());
+      }
+      {
+        json j = json::parse(s);
+        j["path"] = std::filesystem::weakly_canonical(std::filesystem::path(dllDir + std::string(R"EOF(\avrenderer.exe)EOF"))).string();
+        s = j.dump(2);
+      }
+      {    
+        std::ofstream outFile(manifestFilePath);
+        outFile << s;
+        outFile.close();
+      }
+      
+      HKEY hKey;
+      LPCTSTR sk = R"EOF(Software\Google\Chrome\NativeMessagingHosts\com.exokit.xrchrome)EOF";
+      LONG openRes = RegOpenKeyEx(HKEY_CURRENT_USER, sk, 0, KEY_ALL_ACCESS , &hKey);
+      if (openRes == ERROR_FILE_NOT_FOUND) {
+        openRes = RegCreateKeyExA(HKEY_CURRENT_USER, sk, 0, NULL, 0, KEY_ALL_ACCESS, NULL, &hKey, NULL);
+        
+        if (openRes != ERROR_SUCCESS) {
+          getOut() << "failed to create registry key: " << (void*)openRes << std::endl;
+          abort();
+        }
+      } else if (openRes != ERROR_SUCCESS) {
+        getOut() << "failed to open registry key: " << (void*)openRes << std::endl;
+        abort();
+      }
+
+      LPCTSTR value = "";
+      LPCTSTR data = manifestFilePath.c_str();
+      LONG setRes = RegSetValueEx(hKey, value, 0, REG_SZ, (LPBYTE)data, strlen(data)+1);
+      if (setRes != ERROR_SUCCESS) {
+        getOut() << "failed to set registry key: " << (void*)setRes << std::endl;
+        abort();
+      }
+
+      LONG closeRes = RegCloseKey(hKey);
+      if (closeRes != ERROR_SUCCESS) {
+        getOut() << "failed to close registry key: " << (void*)closeRes << std::endl;
+        abort();
+      }
+    }
   }
 
-  std::cerr << "start native host" << std::endl;
-  // std::cout << "start app" << std::endl;
-  for (;;) {
-    uint32_t size;
-    std::cin.read((char *)&size, sizeof(uint32_t));
-    // std::cout << "start app 2 " << size << " " << std::cin.good() << " " << std::cin.bad() << " " << std::cin.fail() << " " << std::cin.eof() << std::endl;
-    if (std::cin.good()) {
-      // std::cout << "start app 3 " << size << std::endl;
-      std::vector<uint8_t> readbuf(size);
-      std::cin.read((char *)readbuf.data(), readbuf.size());
-      // std::cout << "start app 4" << std::endl;
+  {
+    setmode(fileno(stdout), O_BINARY);
+    setmode(fileno(stdin), O_BINARY);
+
+    freopen(NULL, "rb", stdin);
+    freopen(NULL, "wb", stdout);
+
+    char cwdBuf[MAX_PATH];
+    if (!GetCurrentDirectory(
+      sizeof(cwdBuf),
+      cwdBuf
+    )) {
+      getOut() << "failed to get current directory" << std::endl;
+      abort();
+    }
+
+    getOut() << "start native host" << std::endl;
+    for (;;) {
+      uint32_t size;
+      std::cin.read((char *)&size, sizeof(uint32_t));
       if (std::cin.good()) {
-        // std::cout << "start app 5" << std::endl;
-        json req = json::parse(readbuf);
-        // std::cout << "start app 6 " << req.size() << std::endl;
-        json method;
-        json args;
-        for (json::iterator it = req.begin(); it != req.end(); ++it) {
-          // std::cout << it.key() << " " << it.value() << std::endl;
-          if (it.key() == "method") {
-            method = it.value();
-          } else if (it.key() == "args") {
-            args = it.value();
+        std::vector<uint8_t> readbuf(size);
+        std::cin.read((char *)readbuf.data(), readbuf.size());
+        if (std::cin.good()) {
+          json req = json::parse(readbuf);
+          json method;
+          json args;
+          for (json::iterator it = req.begin(); it != req.end(); ++it) {
+            if (it.key() == "method") {
+              method = it.value();
+            } else if (it.key() == "args") {
+              args = it.value();
+            }
           }
-        }
-        
-        if (method.is_string() && args.is_array()) {
-          const std::string methodString = method.get<std::string>();
-          std::cout << "method: " << methodString << std::endl;
+          
+          if (method.is_string() && args.is_array()) {
+            const std::string methodString = method.get<std::string>();
+            getOut() << "method: " << methodString << std::endl;
 
-          /* int i = 0;
-          for (json::iterator it = args.begin(); it != args.end(); ++it) {
-            const std::string argString = it->get<std::string>();
-            std::cout << "arg " << i << ": " << argString << std::endl;
-            i++;
-          } */
-           if (methodString == "addObject" && args.size() > 0 && args[0].is_string()) {
-            std::string argString = args[0].get<std::string>();
+            /* int i = 0;
+            for (json::iterator it = args.begin(); it != args.end(); ++it) {
+              const std::string argString = it->get<std::string>();
+              std::cout << "arg " << i << ": " << argString << std::endl;
+              i++;
+            } */
+             if (methodString == "addObject" && args.size() > 0 && args[0].is_string()) {
+              std::string argString = args[0].get<std::string>();
 
-            std::string name("object");
-            name += std::to_string(++ids);
+              std::string name("object");
+              name += std::to_string(++ids);
 
-            auto model = app->renderer->m_renderer->createModelInstance(name, argString.data(), argString.size());
-            // std::shared_ptr<vkglTF::Model> VulkanExample::findOrLoadModel( std::string modelUri, std::string *psError)
-            
-            json result = {
-              // {"processId", processId}
-            };
-            json res = {
-              {"error", nullptr},
-              {"result", result}
-            };
-            respond(res);
+              auto model = app->renderer->m_renderer->createModelInstance(name, argString.data(), argString.size());
+              // std::shared_ptr<vkglTF::Model> VulkanExample::findOrLoadModel( std::string modelUri, std::string *psError)
+              
+              json result = {
+                // {"processId", processId}
+              };
+              json res = {
+                {"error", nullptr},
+                {"result", result}
+              };
+              respond(res);
+            }
           }
         }
       }
