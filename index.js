acbdc31bfbb970d0af0cd144df5d7765b84f23b6
Avaer Kazmer <a@modules.io>
2020-03-09 02:41:27 -0400

    Add local server

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
+
+const app = express();
+app.use(express.static(__dirname));
+
+http.createServer(app)
+  .listen(3000);
+
+console.log(`http://127.0.0.1:3000`);
