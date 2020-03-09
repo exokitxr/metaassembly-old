f650b894f66c1a5f612657e5285171c5dbd7976e
Avaer Kazmer <a@modules.io>
2020-03-08 22:13:01 -0400

    Add build.ps1

---
+++ build.ps1
@@ -0,0 +1,18 @@
+# build
+echo building...
+
+$ProgressPreference = 'SilentlyContinue'
+
+Invoke-WebRequest "https://github.com/Hibbiki/chromium-win64/releases/download/v79.0.3945.130-r706915/chrome.sync.7z" -OutFile "chrome.7z"
+7z x chrome.7z -aoa
+rm chrome.7z
+
+msbuild Aardvark.sln
+
+echo zipping artifact... 
+7z a avrenderer.zip -r .\src\build\avrenderer\Debug\
+echo done zipping artifact
+
+ls
+
+echo done
