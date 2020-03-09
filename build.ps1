d88523d89ea8739ef821b81fa99588a2e6374816
Avaer Kazmer <a@modules.io>
2020-03-08 22:19:30 -0400

    Add steamworks sdk download

--- build.ps1
+++ build.ps1
@@ -7,6 +7,10 @@ Invoke-WebRequest "https://github.com/Hibbiki/chromium-win64/releases/download/v
 7z x chrome.7z -aoa
 rm chrome.7z
 
+Invoke-WebRequest "https://raw.githubusercontent.com/avaer/swsdk/master/steamworks_sdk_148.zip" -OutFile "steamworks_sdk_148.zip"
+7z x steamworks_sdk_148.zip -aoa
+rm steamworks_sdk_148.zip
+
 msbuild Aardvark.sln
 
 echo zipping artifact... 
