0804bf20aeb0bc7586019e5c3333ac6f83f5977e
Avaer Kazmer <a@modules.io>
2020-03-10 17:38:37 -0400

    Add module render.cpp

---
+++ module/renderer.cpp
@@ -0,0 +1,155 @@
+#define TINYGLTF_IMPLEMENTATION
+#define TINYGLTF_NO_STB_IMAGE_WRITE
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_MSC_SECURE_CRT
+#include "tiny_gltf.h"
+
+#include <tools/logging.h>
+
+#include "av_cef_app.h"
+#include "avserver.h"
+
+#include <chrono>
+#include <thread>
+#include <tools/systools.h>
+#include <tools/pathtools.h>
+#include <tools/stringtools.h>
+
+// XXX
+
+#include <io.h>
+#include <fcntl.h>
+#include <iostream>
+#include <filesystem>
+
+// #define CINTERFACE
+// #define D3D11_NO_HELPERS
+#include <windows.h>
+// #include <D3D11_4.h>
+// #include <DXGI1_4.h>
+// #include <wrl.h>
+
+#include <psapi.h>
+
+// #include "device/vr/detours/detours.h"
+#include "json.hpp"
+
+// #include "device/vr/openvr/test/out.h"
+// #include "third_party/openvr/src/src/vrcommon/sharedlibtools_public.h"
+// #include "device/vr/openvr/test/fake_openvr_impl_api.h"
+#include "base64.h"
+
+#include "javascript_renderer.h"
+#include <glm/gtc/type_ptr.hpp>
+
+#include "out.h"
+#include "file_io.h"
+
+using json = nlohmann::json;
+using Base64 = macaron::Base64;
+
+std::string logSuffix = "_native_host";
+// HWND g_hWnd = NULL;
+// CHAR s_szDllPath[MAX_PATH] = "vrclient_x64.dll";
+std::string dllDir;
+
+inline uint32_t divCeil(uint32_t x, uint32_t y) {
+  return (x + y - 1) / y;
+}
+constexpr uint32_t chunkSize = 1000*1000;
+void respond(const json &j) {
+  std::string outString = j.dump();
+  uint32_t outSize = (uint32_t)outString.size();
+  if (outSize < chunkSize) {
+    std::cout.write((char *)&outSize, sizeof(outSize));
+    std::cout.write(outString.data(), outString.size());
+  } else {
+    uint32_t numChunks = divCeil(outSize, chunkSize);
+    // std::cout << "write chunks " << outSize << " " << chunkSize << " " << numChunks << std::endl;
+    for (uint32_t i = 0; i < numChunks; i++) {
+      // std::cout << "sending " << i << " " << numChunks << " " << outString.substr(i*chunkSize, chunkSize).size() << std::endl;
+      json j2 = {
+        {"index", i},
+        {"total", numChunks},
+        {"continuation", outString.substr(i*chunkSize, chunkSize)},
+      };
+      std::string outString2 = j2.dump();
+      uint32_t outSize2 = (uint32_t)outString2.size();
+      std::cout.write((char *)&outSize2, sizeof(outSize2));
+      std::cout.write(outString2.data(), outString2.size());
+    }
+    // std::cout << "done sending" << std::endl;
+  }
+}
+
+void terminateProcesses(const std::vector<const char *> &candidateFilenames) {
+  char cwdBuf[MAX_PATH];
+  if (!GetCurrentDirectory(sizeof(cwdBuf), cwdBuf)) {
+    getOut() << "failed to get current directory" << std::endl;
+    abort();
+  }
+  std::string cwdBufString(cwdBuf);
+  
+  DWORD aProcesses[1024], cbNeeded, cProcesses;
+  if (EnumProcesses(aProcesses, sizeof(aProcesses), &cbNeeded)) {
+    cProcesses = cbNeeded / sizeof(DWORD);
+
+    bool matchedSelf = false;
+    for (DWORD i = 0; i < cProcesses; i++) {
+      DWORD pid = aProcesses[i];
+      if (pid != 0) {
+        HANDLE h = OpenProcess(
+          PROCESS_QUERY_INFORMATION | PROCESS_VM_READ | PROCESS_TERMINATE,
+          FALSE,
+          pid
+        );
+        if (h) {
+          char p[MAX_PATH];
+          if (GetModuleFileNameExA(h, 0, p, MAX_PATH)) {
+            std::string pString(p);
+            if (pString.rfind(cwdBufString, 0) == 0) {
+              std::string filenameString(p + cwdBufString.length() + 1); // cwd slash
+              bool match = false;
+              for (auto candidateFileName : candidateFilenames) {
+                if (filenameString == candidateFileName) {
+                  if (pid != GetCurrentProcessId()) {
+                    match = true;
+                  } else {
+                    matchedSelf = true;
+                  }
+                  break;
+                }
+              }
+              if (match) {
+                getOut() << "terminate process " << p << " " << pid << std::endl;
+                if (!TerminateProcess(h, 0)) {
+                  getOut() << "failed to terminate process " << p << " " << pid << " " << (void *)GetLastError() << std::endl;
+                }
+              }
+            }
+          } else {
+            getOut() << "failed to get process file name: " << (void *)GetLastError() << std::endl;
+          }
+          CloseHandle(h);
+        }
+      }
+    }
+    if (matchedSelf) {
+      getOut() << "terminating self" << std::endl;
+      ExitProcess(0);
+    }
+  } else {
+    getOut() << "failed to enum chrome processes" << std::endl;
+  }
+}
+void terminateKnownProcesses() {
+  terminateProcesses(std::vector<const char *>{
+    "Chrome-bin\\chrome.exe",
+  });
+  terminateProcesses(std::vector<const char *>{
+    "avrenderer.exe",
+  });
+  /* terminateProcesses(std::vector<const char *>{
+    "av.exe",
+  }); */
+}
