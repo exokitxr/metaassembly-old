5d78e2b1f8118ea1f5558167f53894653bd4c614
Avaer Kazmer <a@modules.io>
2020-03-12 00:48:53 -0400

    Do not decapitate rig by default

--- extension/peerconnection.js
+++ extension/peerconnection.js
@@ -24,7 +24,7 @@ export async function initLocalRig(container) {
     fingers: true,
     hair: true,
     visemes: true,
-    decapitate: true,
+    decapitate: false,
     microphoneMediaStream: null,
     // debug: !newModel,
   });
