4319b1b27ea642da7e556de192f808682031c8ab
Avaer Kazmer <a@modules.io>
2020-03-16 03:49:53 -0400

    Logging

--- extension/peerconnection.js
+++ extension/peerconnection.js
@@ -194,6 +194,7 @@ export function bindPeerConnection(peerConnection, container) {
     if (videoTracks.length > 0) {
       peerConnection.screenshareMediaStream = e.detail;
     }
+    console.log('got tracks', audioTracks, videoTracks, peerConnection);
     if (peerConnection.rig) {
       if (audioTracks.length > 0) {
         peerConnection.rig.setMicrophoneMediaStream(peerConnection.microphoneMediaStream, {
@@ -201,6 +202,7 @@ export function bindPeerConnection(peerConnection, container) {
         });
       }
       if (videoTracks.length > 0) {
+        console.log('dispatch screen share', peerConnection.screenshareMediaStream);
         peerConnection.dispatchEvent(new CustomEvent('screenshare', {
           detail: peerConnection.screenshareMediaStream,
         }));
