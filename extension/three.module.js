0aa4d57a9ead446b74d8f1c3e67dbf7ced311991
Avaer Kazmer <a@modules.io>
2020-03-16 00:10:11 -0400

    Add three.js getFrameBuffer method

--- extension/three.module.js
+++ extension/three.module.js
@@ -26191,6 +26191,7 @@ function WebGLRenderer( parameters ) {
 	}
 
 	//
+	this.getFramebuffer = function() { return _framebuffer; };
 	this.setFramebuffer = function ( value ) {
 
 		if ( _framebuffer !== value && _currentRenderTarget === null ) _gl.bindFramebuffer( 36160, value );
